562dc8a8a467108856f6599de58c7d420c258f99
Ronda Bergman <rondabergman@hotmail.com>
2017-03-21 21:00:11 -0400

    initial commit

---
+++ PrismDemoSolution/MyModule/MyModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+
+namespace MyModule
+{
+    public class MyModule : IModule
+    {
+        private readonly IRegionViewRegistry regionViewRegistry;
+
+        public MyModule(IRegionViewRegistry registry)
+        {
+            this.regionViewRegistry = registry;
+        }
+
+        public void Initialize()
+        {
+            regionViewRegistry.RegisterViewWithRegion("MyRegion", typeof(Views.MyModule));
+        }
+    }
+}
